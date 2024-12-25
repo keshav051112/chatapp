6c5612dd34cba8104d30fd768cd1a1e9a918a620
keshav051112 <160417729+keshav051112@users.noreply.github.com>
2024-12-26 02:27:34 +0530

    Updated the database connection logic

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async()=>{
     try{
-        await mongoose.connect('mongodb://localhost:27017/chatapps')
+        await mongoose.connect(process.env.MONGODB_URI )
         console.log('MongoDB Connected')
     }catch(err){
         console.error(err)
