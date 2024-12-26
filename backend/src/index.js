3fd04052a0fa56134a2379b7826686efef55aaa8
keshav051112 <160417729+keshav051112@users.noreply.github.com>
2024-12-26 09:43:24 +0530

    cors allowed

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,10 +14,7 @@ app.use(express.json({ limit: '10mb' }));
 
 app.use(cookieParser())
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(cors());
 
 dotenv.config()
 const PORT = process.env.PORT;
